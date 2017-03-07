f80689cc363fe3895b8981de6faa74eb2dcf6107
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-07 13:12:21 -0500

    Create resizing int set

---
+++ hashmap/lib/p01_int_set.rb
@@ -0,0 +1,102 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num < @store.length && num > 0
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    return if include?(num)
+    resize! if count >= num_buckets
+    @count += 1
+    self[num] << num
+  end
+
+  def remove(num)
+    return unless include?(num)
+    self[num].delete(num)
+    @count -= 1
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store.flatten
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.each { |num| insert(num) }
+  end
+end
