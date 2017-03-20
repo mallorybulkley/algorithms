099e4e798416cd643232533eb0502908f12677bd
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-20 12:10:45 -0400

    Update static array

--- dynamic-array/lib/dynamic_array.rb
+++ dynamic-array/lib/dynamic_array.rb
@@ -4,23 +4,36 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new(4)
+    @length = 0
+    @capacity = 4
   end
 
   # O(1)
   def [](index)
+    raise "index out of bounds" if index >= length
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    raise "index out of bounds" if index >= length
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    popped = @store[length - 1]
+    @store[length - 1] = nil
+    @length -= 1
+    popped
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if length >= capacity
+    @store[length]
   end
 
   # O(n): has to shift over all the elements.
