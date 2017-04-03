5b9bfbc218ee995b74d938d0e23b5505a723b621
Mallory Bulkley <mallorybulkley@gmail.com>
2017-04-03 09:45:51 -0400

    Add more string problems

--- arrays-and-strings/problems.rb
+++ arrays-and-strings/problems.rb
@@ -16,3 +16,35 @@ def is_unique_no_additional_data_structures(string)
 
   true
 end
+
+# 1.2 Check Permutation
+def check_permutation(string1, string2)
+  string1.split("").sort == string2.split("").sort
+end
+
+def check_perm_char_count(string1, string2)
+  counts = Hash.new(0)
+
+  string1.each_char do |char|
+    counts[char] += 1
+  end
+
+  string2.each_char do |char|
+    counts[char] -= 1
+  end
+
+  counts.none? { |char, count| count != 0 }
+end
+
+def urlify(string)
+  string.gsub(" ", "%20")
+end
+
+# O(n) time - must look at every letter
+def palindrome_perm(string)
+  letters = Hash.new(0)
+  string.each_char { |char| letters[char] += 1 unless char == " " }
+  odd_counts = letters.select { |_, v| v.odd? }
+  return false if odd_counts.length > 1
+  true
+end
